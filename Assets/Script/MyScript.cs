b6dc0d4db7e6d6560568ed683f140db580a5a20d
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-06-01 19:54:21 +0700

    add file

--- Assets/Script/MyScript.cs
+++ Assets/Script/MyScript.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class MyScript : MonoBehaviour
 {
+
+    public float speed = 10.0f;
+    private Rigidbody rb;
+    public Vector3 movement;        //รับอินพุตจากผู้ใช้
+      
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Start Running");
-    }
-
-    private void Awake()
-    {
-        Debug.Log("Awake Running");
+        rb = GetComponent<Rigidbody>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //รับค่าจากแป้นพิมพ์
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Jump");
+        float z = Input.GetAxis("Vertical");
     }
+
 }
